8414679320b3d1a62914afbee62b289820c0536f
Nurane1346 <nuraneh01@gmail.com>
2024-02-02 19:15:53 +0400

    created file

---
+++ main.js
@@ -0,0 +1,121 @@
+
+// Task 1
+
+// function name(ad){
+//     console.log(ad)
+// }
+// name("Nurane");
+// name("Lale");
+// name("Nermin");
+
+// Task 2
+
+// function eded(a,b,c){
+//    if(a>b && a>c){
+//       console.log(a);
+//    } else if(b>a && b>c){
+//       console.log(b)
+//    } else if(c>a && c>b){
+//       console.log(c)
+//    }
+// }
+// eded(3,7,9);
+
+// Task 3
+// let result = hesab(1200,20);
+// console.log(result);
+// function hesab(a,b){
+//    let cem =a+(75*b);
+//   return cem;
+// }
+
+// Task 4
+
+// function numberToText(reqem) {
+//    switch (reqem) {
+//       case 1:
+//          number = "bir"
+//          break;
+
+//       case 2:
+//          number = "iki"
+//          break;
+//       case 3:
+//          number = "uc"
+//          break;
+//       case 4:
+//          number = "dord"
+//          break;
+
+//       case 5:
+//          number = "bes"
+//          break;
+//       case 6:
+//          number = "alti"
+//          break;
+//       case 7:
+//          number = "yeddi"
+//          break;
+//       case 8:
+//          number = "sekkiz"
+//          break;
+//       case 9:
+//          number = "doqquz"
+//          break;
+
+//    }
+//    console.log("on" + day);
+
+
+// }
+// numberToText(8);
+
+
+
+// Task 5
+
+// let result = min(2,5);
+// function min(a,b){
+//    return a<b ? a:b;
+// }
+// console.log(result);
+
+// Task 6
+
+// function calc(a, b, c) {
+//    let result;
+//    switch (c) {
+//       case "+":
+//          result = a + b;
+//          break;
+//       case "-":
+//          result = a - b;
+//          break;
+//       case "*":
+//          result = a * b;
+//          break;
+//       case "/":
+//          result = a / b;
+//          break;
+//    }
+//    console.log(result);
+// }
+
+// calc(30,5,"+");
+// calc(30,5,"-");
+// calc(30,5,"*");
+// calc(30,5,"/");
+
+// Task 7
+
+// function isLucky(param){
+//    let strnomre = string(param)
+//    let first = +strnomre[0] + +strnomre[1] + +strnomre[2]
+//    let second = +strnomre[3] + +strnomre[4] + +strnomre[5]
+//    if(first===second){
+//       console.log("true")
+//    }else{
+//       console.log("false")
+//    }
+// }
+// console.log(isLucky(234801));
